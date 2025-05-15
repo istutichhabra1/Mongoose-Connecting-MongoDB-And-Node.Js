95497438bf00dc3738e758bda960a38519a9253e
Istuti Chhabra <102655463+istutichhabra1@users.noreply.github.com>
2025-05-15 12:17:41 +0530

    Create tasks.js

---
+++ ques1/routes/tasks.js
@@ -0,0 +1,51 @@
+
+const express = require("express");
+const Task = require("../models/Task");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    await task.save();
+    res.status(201).json(task);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+
+router.get("/", async (req, res) => {
+  try {
+    const { status, dueDate } = req.query;
+    const filter = {};
+    if (status) filter.status = status;
+    if (dueDate) filter.dueDate = new Date(dueDate);
+    const tasks = await Task.find(filter);
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Update Task
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedTask) return res.status(404).json({ error: "Task not found" });
+    res.json(updatedTask);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedTask = await Task.findByIdAndDelete(req.params.id);
+    if (!deletedTask) return res.status(404).json({ error: "Task not found" });
+    res.json({ message: "Task deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
